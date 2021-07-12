f8cef4cd9e2e20f6a6401497ca0457c82d678484
gjhhust <gjh_hust@foxmail.com>
2021-07-12 18:45:51 +0800

    整理两个传感器完毕（f103RC）
    1.将中断函数整理
    2.将TIME_SAVE变量修改去掉了多余的标志位

--- 01_SMT32F103RCT6空白模板(已配参数)/User/speedtext/speedtext.h
+++ 01_SMT32F103RCT6空白模板(已配参数)/User/speedtext/speedtext.h
@@ -21,14 +21,13 @@ void InitAll(void);//全部硬件初始化
 typedef struct
 {   
 	uint32_t  first_time;     // 第一次时间
-	uint8_t		first_start; //完成标志位
-	uint8_t		first_finishing; //完成标志位
+	uint32_t first_finishing;
+	uint32_t		second_time; 
+	uint32_t second_finishing;
 	
-	uint32_t  second_time;    // 第二次时间
-	uint8_t		second_start; //完成标志位
-	uint8_t		second_finishing; //完成标志位
+	uint32_t  Interval_time12;    // 间隔时间
 	
-	uint32_t  Interval_time;    // 间隔时间
+	
 	
 	uint32_t states;//1.2.3.4
 }USER;
